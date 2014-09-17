4e0f509d1f6683907b554845636060130da47e73
Jeremy Gruszka <C16Jeremy.Gruszka@usafa.edu>
2014-09-16 21:11:48 -0600

    second commit - required functionality

--- main.asm
+++ main.asm
@@ -2,7 +2,8 @@
 ; Lab 2
 ; Jeremy Gruszka, 15 SEP 2014
 ;
-; Encrypter --
+; Encrypter -- Takes in a message, uses a key to encrpyt the message, then uses
+;			   the same key to decrypt the message.
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
 
@@ -13,56 +14,72 @@
             .retainrefs                     ; Additionally retain any sections
                                             ; that have references to current
                                             ; section
+
+message		.byte	0xef,0xc3,0xc2,0xcb,0xde,0xcd,0xd8,0xd9,0xc0,0xcd,0xd8,0xc5,0xc3,0xc2,0xdf,0x8d,0x8c,0x8c,0xf5,0xc3,0xd9,0x8c,0xc8,0xc9,0xcf,0xde,0xd5,0xdc,0xd8,0xc9,0xc8,0x8c,0xd8,0xc4,0xc9,0x8c,0xe9,0xef,0xe9,0x9f,0x94,0x9e,0x8c,0xc4,0xc5,0xc8,0xc8,0xc9,0xc2,0x8c,0xc1,0xc9,0xdf,0xdf,0xcd,0xcb,0xc9,0x8c,0xcd,0xc2,0xc8,0x8c,0xcd,0xcf,0xc4,0xc5,0xc9,0xda,0xc9,0xc8,0x8c,0xde,0xc9,0xdd,0xd9,0xc5,0xde,0xc9,0xc8,0x8c,0xca,0xd9,0xc2,0xcf,0xd8,0xc5,0xc3,0xc2,0xcd,0xc0,0xc5,0xd8,0xd5,0x8f
+
+key:		.byte	0xac					;key used for encryption/decryption
+
 ;-------------------------------------------------------------------------------
+
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 ;-------------------------------------------------------------------------------
-                                            ; Main loop here
+; Main loop here
 ;-------------------------------------------------------------------------------
 
-            ;
-            ; load registers with necessary info for decryptMessage here
-            ;
+			mov.w	#message, R4            ;
+            mov.w	#0x0200, R10			; load registers with necessary info for decryptMessage here
+            								;
 
             call    #decryptMessage
 
 forever:    jmp     forever
 
 ;-------------------------------------------------------------------------------
-                                            ; Subroutines
+; Subroutines
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
 ;Subroutine Name: decryptMessage
-;Author:
+;Author: Jeremy Gruszka
 ;Function: Decrypts a string of bytes and stores the result in memory.  Accepts
 ;           the address of the encrypted message, address of the key, and address
 ;           of the decrypted message (pass-by-reference).  Accepts the length of
 ;           the message by value.  Uses the decryptCharacter subroutine to decrypt
 ;           each byte of the message.  Stores theresults to the decrypted message
 ;           location.
-;Inputs:
+;Inputs:	R4 (message), R11 (counter)
 ;Outputs:
 ;Registers destroyed:
 ;-------------------------------------------------------------------------------
 
 decryptMessage:
 
+			mov.w	#0x5E, R11			;counter for length of message
+			mov.b	@R4+, R5			;moves next part of message into register
+			call	#decryptCharacter
+			mov.b	R5, 0(R10)			;stores decrypted message in memory
+			inc.w	R10
+			dec.w	R11
+			jnz		decryptMessage
+
             ret
 
 ;-------------------------------------------------------------------------------
 ;Subroutine Name: decryptCharacter
-;Author:
+;Author: Jeremy Gruszka
 ;Function: Decrypts a byte of data by XORing it with a key byte.  Returns the
 ;           decrypted byte in the same register the encrypted byte was passed in.
 ;           Expects both the encrypted data and key to be passed by value.
-;Inputs:
-;Outputs:
-;Registers destroyed:
+;Inputs:	R5 (character)
+;Outputs:	R5 (decrypted character)
+;Registers destroyed:	R5
 ;-------------------------------------------------------------------------------
 
 decryptCharacter:
+
+			xor		key, R5				;decrypts character
 
             ret
 
